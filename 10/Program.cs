a7e6cb3c8abf55a30fb8017f64669fb93fcd0541
Nader Jafari <JBritannia@proton.me>
2024-12-10 23:15:22 +0330

    Catching up

---
+++ 10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Immutable;
+
+int L = _10.Resource.Input.IndexOf('\n');
+Direction[] AllDirections = [Direction.Left, Direction.Right, Direction.Up, Direction.Down];
+int[,] map = new int[L, L];
+HashSet<List<(int x, int y)>> paths = new();
+for (int i = 0; i < L; i++)
+	for (int j = 0; j < L; j++) {
+		map[i, j] = Convert.ToInt32(_10.Resource.Input[i * (L + 1) + j]) - 48;
+		if (map[i, j] == 0)
+			paths.Add(new([(i, j)]));
+	}
+
+foreach (var path in paths.ToImmutableList()) {
+	PathFind(path, AllDirections);
+}
+paths.RemoveWhere(l => l.Count != 10);
+Console.WriteLine(paths.GroupBy(l => l.First()).Select(g => g.GroupBy(l => l.Last()).Count()).Sum());
+Console.WriteLine(paths.GroupBy(l => l.First()).Select(g => g.Count()).Sum());
+void PathFind(List<(int x, int y)> l, params Direction[] directions) {
+	foreach (var direction in directions) {
+		var cur = l.Last();
+		if (map[cur.x, cur.y] == 9)
+			return;
+		var next = Next(cur, direction);
+		if (!IsValid(next) || map[next.x, next.y] - map[cur.x, cur.y] != 1)
+			continue;
+		List<(int x, int y)> newl = new(l) {
+			next
+		};
+		paths.Add(newl);
+		PathFind(newl, AllDirections.Where(d => d != ~direction).ToArray());
+	}
+}
+bool IsValid((int x, int y) p) => !(p.x < 0 || p.y < 0 || p.x >= L || p.y >= L);
+(int x, int y) Next((int x, int y) p, Direction d) => d switch {
+	Direction.Left => (p.x, p.y - 1),
+	Direction.Right => (p.x, p.y + 1),
+	Direction.Up => (p.x - 1, p.y),
+	Direction.Down => (p.x + 1, p.y),
+	_ => p
+};
+enum Direction {
+	Left = 0b0101, Right = 0b1010, Up = 0b1100, Down = 0b0011
+}
---
+++ 9/Program.cs
@@ -0,0 +1,66 @@
+﻿int[] files = new int[_9.Resource.Input.Length / 2 + 1];
+int[] free = new int[_9.Resource.Input.Length / 2];
+List<Chunk> chunks = new(_9.Resource.Input.Length);
+for (int i = 0; i < _9.Resource.Input.Length; i++) {
+	if ((i & 1) == 1) {
+		free[i / 2] = Convert.ToInt32(_9.Resource.Input[i]) - 48;
+		chunks.Add(new() { Id = -1, Size = free[i / 2] });
+	} else {
+		files[i / 2] = Convert.ToInt32(_9.Resource.Input[i]) - 48;
+		chunks.Add(new() { Id = i / 2, Size = files[i / 2] });
+	}
+}
+int forwardIndexFiles = 0;
+int reverseIndexFiles = files.Length - 1;
+int forwardIndexFree = 0;
+long checksum1 = 0;
+for (int position = 0; files.AsParallel().Any(x => x != 0); position++) {
+	if (files[forwardIndexFiles] != 0) {
+		checksum1 += position * forwardIndexFiles;
+		files[forwardIndexFiles]--;
+		continue;
+	}
+	if (free[forwardIndexFree] != 0) {
+		if (files[reverseIndexFiles] == 0)
+			reverseIndexFiles--;
+		checksum1 += position * reverseIndexFiles;
+		files[reverseIndexFiles]--;
+		free[forwardIndexFree]--;
+		continue;
+	}
+	forwardIndexFree++;
+	forwardIndexFiles++;
+	position--;
+}
+Console.WriteLine(checksum1);
+for (int i = chunks.Count - 1; i >= 0; i--) {
+	if (chunks[i].Id == -1 || chunks[i].Moved)
+		continue;
+	int j;
+	for (j = 0; j < i; j++)
+		if (chunks[j].Id == -1 && chunks[j].Size >= chunks[i].Size)
+			break;
+	if (j == i)
+		continue;
+	var freeSpace = chunks[j].Size;
+	chunks[j] = chunks[i] with { Moved = true };
+	chunks[i] = chunks[i] with { Id = -1 };
+	if (freeSpace != chunks[j].Size)
+		chunks.Insert(j + 1, new() { Id = -1, Size = freeSpace - chunks[j].Size });
+	i++;
+}
+long checksum2 = 0;
+int pos = 0;
+foreach (var chunk in chunks) {
+	for (int i = 0; i < chunk.Size; i++) {
+		if (chunk.Id != -1)
+			checksum2 += pos * chunk.Id;
+		pos++;
+	}
+}
+Console.WriteLine(checksum2);
+struct Chunk {
+	public int Id;
+	public int Size;
+	public bool Moved;
+}
